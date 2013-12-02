659afe6b133805f3bd85b4c2b68ae2fd6052020d
Dan Bender <mail@danbender.net>
2013-12-01 18:19:51 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImportChallenge::Application.config.secret_key_base = '0a5e2a8621db1b5462bedb20b9d66df7efb835a956d72b42141877639363aa2cd9e0b09b70675500e10bef6e43e6eb6505ec347ec909cae99e7a74912af369b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImportChallenge::Application.config.session_store :cookie_store, key: '_import_challenge_session'
